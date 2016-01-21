72a1edfb21209046861d47869557b369aac3f6fd
cv <cv.ryn7@gmail.com>
2016-01-20 22:48:57 -0500

    added eventlistener to detect click on body

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
 var current_url = window.location.href;
-
+document.body.addEventListener('click',function(){
+    current_url = window.location.href;
+    alert(current_url);
+},true);
 alert(current_url);
